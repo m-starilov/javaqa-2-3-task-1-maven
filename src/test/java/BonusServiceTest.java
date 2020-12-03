3b30b2fe9ba11dea388a5af943f3db71cb8e4dde
Mikhail Starilov <starilov.m@gmail.com>
2020-12-03 16:12:05 +0600

    Tests added

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -11,12 +11,33 @@ class BonusServiceTest {
 
         assertEquals(expected, actual);
     }
+
     @org.junit.jupiter.api.Test
     void shouldCalculateNotRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
         long actual = service.calculate(1000_60, false);
         long expected = 10;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long actual = service.calculate(800000_00, true);
+        long expected = 500;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateNotRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long actual = service.calculate(800000_00, false);
+        long expected = 500;
 
         assertEquals(expected, actual);
     }
